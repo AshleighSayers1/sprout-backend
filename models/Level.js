8cb1bb7b0613d27efbeccc6f15dca934d11a2c3b
AshleighSayers1 <ashleigh.sayers01@hotmail.com>
2021-11-03 16:19:56 +0800

    main push commit

---
+++ models/Level.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const Utils = require('./../utils')
+require('mongoose-type-email')
+
+// schema
+const levelSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  description: {
+    type: String    
+  },
+  level: {
+    type: Number,
+    required: true    
+  },
+  tasks: {
+    type: Array,
+  }
+}, { timestamps: true })
+
+
+// model
+const levelModel = mongoose.model('Level', levelSchema, 'levels')
+
+// export
+module.exports = levelModel
+
+
+
+
